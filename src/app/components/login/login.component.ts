20b09ed36b52cb400d98b29ec07fa3c9cd566c35
Henvidas99 <henryvartiel99@gmail.com>
2023-06-24 13:38:22 -0600

    login, registro

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,12 @@
 //import { Component } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { Usuario } from 'src/app/models/usuario';
+import{ UsuarioService } from 'src/app/services/usuario.service';
+import{timer} from'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { HttpErrorResponse } from '@angular/common/http';
 
 
 @Component({
@@ -20,9 +26,59 @@ import { trigger, transition, style, animate } from '@angular/animations';
   ]
 })
 export class LoginComponent {
+  public usuario:Usuario;
+  public status:number;
+  public remember:boolean;
   mostrarContrasena: boolean = false;
+
+  constructor(
+    private _usuarioService:UsuarioService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ){
+    this.status=-1;
+    this.usuario= new Usuario();
+    this.remember = false;
+  }
 
   toggleMostrarContrasena() {
     this.mostrarContrasena = !this.mostrarContrasena;
+  }
+
+
+  onSubmit(form:any){
+    if(this.remember){
+    }
+
+    this._usuarioService.login(this.usuario).subscribe({
+      next:(response:any)=>{
+        if(response.status!=401){
+          console.log(response);
+          localStorage.setItem("token",response);
+          this._usuarioService.getIdentityFromAPI().subscribe({
+            next:(response:any)=>{ 
+              Swal.fire('¡Login exitoso!', response.message, 'success');            
+              localStorage.setItem("identity",JSON.stringify(response));
+            },
+            error:(err:Error)=>{
+    
+            }
+          });
+          this._router.navigate(['/home']);
+    
+        }else{
+          this.status=0;
+          let counter=timer(3000);
+          counter.subscribe(n=>{
+            this.status=-1;
+          });
+          Swal.fire('¡Error!', 'Usuario y/o contraseña inválidos', 'error');  
+        }
+      },
+      error:(err:Error)=>{
+        console.log(err);
+      }
+    });
+
   }
 }
--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,6 +1,14 @@
 //import { Component } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
+import { Usuario } from 'src/app/models/usuario';
+import { Cliente } from 'src/app/models/cliente';
+import{ ClienteService } from 'src/app/services/cliente.service';
+import{ UsuarioService } from 'src/app/services/usuario.service';
+import{timer} from'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-registro',
@@ -19,5 +27,54 @@ import { trigger, transition, style, animate } from '@angular/animations';
   ]
 })
 export class RegistroComponent {
+  public usuario:Usuario;
+  public cliente:Cliente;
+  public status:number;
+  public fechaFormateada:any;
+  datosPersonalesEnviados: boolean = false;
 
+  constructor(
+    private _clienteService:ClienteService,
+    private _usuarioService:UsuarioService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ){
+    this.status=-1;
+    this.usuario= new Usuario();
+    this.cliente= new Cliente();
+  }
+
+  enviarDatosPersonales(form:any) {
+      console.log(this.cliente);
+      this.fechaFormateada = this.cliente.fechaNac.toString();//cambiar formato de la fecha que da el componente DatePicker
+      this.cliente.fechaNac = this.fechaFormateada;
+      this._clienteService.register(this.cliente).subscribe({
+        next:(response:any)=>{
+          console.log(response.message);
+          Swal.fire('¡Registro guardado!', response.message, 'success');
+          this.datosPersonalesEnviados = true;
+        },
+        error:(err:HttpErrorResponse)=>{
+          //console.log(err.error.message);
+          Swal.fire('¡Error!', err.error.message + ', favor verifica los datos y vuelve a intentarlo', 'error');
+        }
+      });
+    }
+  
+  enviarDatosUsuario(form:any) {
+    console.log(this.usuario);
+    this.usuario.tipoUsuario='cliente';
+    this.usuario.cliente = this.cliente.cedula;
+    this._usuarioService.register(this.usuario).subscribe({
+      next:(response:any)=>{
+        console.log(response.message);
+        Swal.fire('¡Registro guardado!', response.message, 'success');
+      },
+      error:(err:HttpErrorResponse)=>{
+        //console.log(err.error.message);
+        Swal.fire('¡Error!', err.error.message + ', favor verifica los datos y vuelve a intentarlo', 'error');
+      }
+    });
+  }
 }
+
